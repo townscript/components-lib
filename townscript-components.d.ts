02d10c407f5ad647f34fd5ed063919f6f392a138
sumedhmohaneTS <sumedh.mohane@townscript.com>
2019-10-29 13:09:47 +0530

    saj

--- townscript-components.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public_api';
-export { FooterService as ɵa } from './src/app/modules/layout/components/ts-footer/ts-footer.service';
