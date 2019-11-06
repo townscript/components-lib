0bc92d8fdc5d1a1f07ed6c8ec0b70ebdaad2325d
sumedhmohaneTS <sumedh.mohane@townscript.com>
2019-11-06 12:59:10 +0530

    Deplyong

--- src/app/shared/pipes/ts-date-range.pipe.d.ts
+++
@@ -1,5 +0,0 @@
-import { PipeTransform } from '@angular/core';
-export declare class RangeDatePipe implements PipeTransform {
-    days: any;
-    transform: (rangeDates: any, isRecurrent?: any, args?: any) => any;
-}
