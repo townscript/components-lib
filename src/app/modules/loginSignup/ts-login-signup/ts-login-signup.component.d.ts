31d87f0765775f08566829388cd4dd9ee18e26d3
sumedhmohaneTS <sumedh.mohane@townscript.com>
2019-11-13 16:08:07 +0530

    Enhancements

--- src/app/modules/loginSignup/ts-login-signup/ts-login-signup.component.d.ts
+++ src/app/modules/loginSignup/ts-login-signup/ts-login-signup.component.d.ts
@@ -45,6 +45,7 @@ export declare class TsLoginSignupComponent implements OnInit, OnDestroy {
     subObject: any;
     showConfirmation: boolean;
     baseUrl: any;
+    userName: any;
     constructor(cookieService: CookieService, userService: UserService, notificationService: NotificationService, tsLoginSignupService: TsLoginSignupService, placeService: PlaceService);
     ngOnInit(): void;
     ngOnDestroy(): void;
