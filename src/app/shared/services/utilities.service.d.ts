fa869a4b0ab2014ef78e524ace09b7bc8904240e
sumedhmohaneTS <sumedh.mohane@townscript.com>
2019-12-27 20:53:52 +0530

    code refactored

--- src/app/shared/services/utilities.service.d.ts
+++ src/app/shared/services/utilities.service.d.ts
@@ -1,5 +1,6 @@
 export declare class UtilityService {
     FB_APP_ID: any;
+    deprecatedVsNewTimeZones: any;
     constructor();
     IsJsonString: (str: any) => boolean;
     addFBSDK: () => void;
