a27312fad44ffa8bd7a5b09e9a2dd81e84d0722e
sumedhmohaneTS <sumedh.mohane@townscript.com>
2019-12-27 17:16:52 +0530

    date range pipe changes

--- src/app/shared/pipes/ts-date-range.pipe.d.ts
+++ src/app/shared/pipes/ts-date-range.pipe.d.ts
@@ -1,5 +1,5 @@
 import { PipeTransform } from '@angular/core';
 export declare class RangeDatePipe implements PipeTransform {
     days: any;
-    transform: (rangeDates: any, isRecurrent?: any, args?: any) => any;
+    transform: (rangeDates: any, eventTimeZone: any, isRecurrent?: any, args?: any) => any;
 }
