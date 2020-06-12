6101a98d327eedd35551eb01824e8e6bbd09e694
Sushant Pawar <sushant@townscript.com>
2020-06-12 16:50:30 +0530

    fix last build

---
+++ src/app/modules/layout/components/ts-header/ts-header.service.d.ts
@@ -0,0 +1,11 @@
+import { HttpClient } from '@angular/common/http';
+export declare class HeaderService {
+    private http;
+    baseUrl: string;
+    apiServerUrl: string;
+    listingsServerUrl: string;
+    constructor(http: HttpClient);
+    getplaceSearchResults: (query: any) => import("rxjs").Observable<Object>;
+    getPopularCities(countryCode: any): Promise<Object>;
+    getSuggestions(searchText: string): Promise<any>;
+}
