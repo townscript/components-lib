d677ee444cdc4c9821477b69bd81769482da61e8
arpit-twnscrpt <arpit.sharma@townscript.com>
2020-06-25 18:06:21 +0530

    search url change

--- townscript-components.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public_api';
-export { TsListingEventCardComponent as ɵe } from './src/app/modules/cards/ts-listings-event-card/ts-listings-event-card.component';
-export { FooterService as ɵa } from './src/app/modules/layout/components/ts-footer/ts-footer.service';
-export { SearchSuggestionComponent as ɵb } from './src/app/modules/layout/components/ts-header/search-suggestion/search-suggestion.component';
-export { ConfirmationSVGComponent as ɵd } from './src/app/modules/loginSignup/ts-login-signup/confirmation-svg/confirmation-svg.component';
-export { SharedService as ɵc } from './src/app/shared/services/shared.service';
