e50899a1cc86e92234a0af0a23fbaaaf7eece1ca
root <root@ip-172-31-1-178.ap-south-1.compute.internal>
2022-02-16 09:21:58 +0000

    updated insta link

--- src/app/shared/services/time.service.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class TimeService {
-    constructor();
-    convertDateToTimezone: (date: any, timeZoneOffset: any) => any;
-    formatLocalDate: (now: any) => any;
-    dateTimeWithinHours: (date: Date, hours: number) => boolean;
-    nextOccurenceFromRRule: (startDate: Date, endDate: Date, rruleString: string, recTime: string) => Date;
-}
